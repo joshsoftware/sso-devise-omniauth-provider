ca11642e86a670b440a4bda56a3c8bef9565d709
herrBeesch <andreasbeuge@me.com>
2012-08-10 14:30:47 +0300

    Update db/migrate/20101007181515_create_users.rb
    modifications to work with rails 3.2.6

--- db/migrate/20101007181515_create_users.rb
+++ db/migrate/20101007181515_create_users.rb
@@ -1,11 +1,24 @@
 class CreateUsers < ActiveRecord::Migration
   def self.up
     create_table :users do |t|
-      t.database_authenticatable :null => false
-      t.recoverable
-      t.rememberable
-      t.trackable
-      t.token_authenticatable
+      ## Database authenticatable
+      t.string :email,              :null => false, :default => ""
+      t.string :encrypted_password, :null => false, :default => ""
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      ## Trackable
+      t.integer  :sign_in_count, :default => 0
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+      
 
       t.boolean :admin, :default => false
       t.string :first_name
